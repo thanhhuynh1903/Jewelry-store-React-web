66cd56db90e468e4e4d3d91204a1f93c8b91d3f0
Trung Tin <duongtrungtinst127@gmail.com>
2024-06-16 17:05:32 +0700

    fix conflict

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,11 +3,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
-<<<<<<< HEAD
 import Home from "../src/components/homepage/Home";
-=======
 import 'react-toastify/dist/ReactToastify.css';
->>>>>>> f885bfedbb599130cba9963461f045dc84f30186
 
 const App = () => {
   return (
