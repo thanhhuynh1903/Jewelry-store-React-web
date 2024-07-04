adb731f025bc4cfeb0a7b29ffd87d3e7d780d837
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-07-04 21:51:11 +0700

    add toast display

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -9,22 +9,27 @@ import GoogleCallback from "hook/GoogleCallback";
 import Home from "../src/components/homepage/Home";
 import ProductDetail from "components/products/ProductDetail";
 import Cart from "components/cart/Cart";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/auth" replace />} />
-      <Route path="auth/*" element={<AuthLayout />} />
-      <Route path="/auth/google/callback" element={<GoogleCallback />} /> {/* Add the callback route */}
-      <Route element={<HasAuth />}>
-        <Route path="admin/*" element={<AdminLayout />} />
-        <Route path="rtl/*" element={<RtlLayout />} />
-        <Route path="home/*" element={<Home/>} />
-      </Route>
-      <Route path="/product/:id" element={<ProductDetail />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth" replace />} />
+        <Route path="auth/*" element={<AuthLayout />} />
+        <Route path="/auth/google/callback" element={<GoogleCallback />} />{" "}
+        {/* Add the callback route */}
+        <Route element={<HasAuth />}>
+          <Route path="admin/*" element={<AdminLayout />} />
+          <Route path="rtl/*" element={<RtlLayout />} />
+          <Route path="home/*" element={<Home />} />
+        </Route>
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
 
