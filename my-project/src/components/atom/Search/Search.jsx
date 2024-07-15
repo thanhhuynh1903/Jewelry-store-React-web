bdba429ce7f97913f206dba81ecc666bdf470851
thanhhuynh1903 <thanhgaming1234@gmail.com>
2024-07-15 22:58:14 +0700

    fix name

--- my-project/src/components/atom/Search/Search.jsx
+++ my-project/src/components/atom/Search/Search.jsx
@@ -15,7 +15,7 @@ const Search = ({ onSearch }) => {
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Search customer ..."
+        placeholder="Search ..."
         className="px-4 py-2 border rounded"
       />
       <button type="submit" className="ml-2 px-4 py-2 bg-blue-500 text-white rounded">
